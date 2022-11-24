d29dae8fba58552db1b91ed0cd23ed06c7f29c93
Selchuk Karakus <selchuk.karakus@gmail.com>
2022-11-24 22:23:24 +0000

    chore: Add subtract, multiply and divide functions to pass test

--- example.js
+++ example.js
@@ -1,5 +1,22 @@
 const sum = (firstNum, secondNum) => {
-  return firstNum + secondNum
-}
+  return firstNum + secondNum;
+};
 
-module.exports = sum;
+const subtract = (firstNum, secondNum) => {
+  return firstNum - secondNum;
+};
+
+const multiply = (firstNum, secondNum) => {
+  return firstNum * secondNum;
+};
+
+const divide = (firstNum, secondNum) => {
+  return firstNum / secondNum;
+};
+
+module.exports = {
+  sum,
+  subtract,
+  multiply,
+  divide,
+};
