a6c56874634dc6911c0d8d1ebb1ed4586d42070c
Selchuk Karakus <selchuk.karakus@gmail.com>
2022-11-24 22:21:46 +0000

    test: Create two test suites

--- example.test.js
+++ example.test.js
@@ -1,5 +1,21 @@
-const sum = require("./example");
+const { sum, subtract, multiply, divide } = require("./example");
 
-test("adds 1 + 2 to equal 3", () => {
-  expect(sum(1, 2)).toBe(3);
+describe("additive functions", () => {
+  test("adds numbers correctly", () => {
+    expect(sum(10, 20)).toBe(30);
+  });
+
+  test("subtracts numbers correctly", () => {
+    expect(subtract(20, 20)).toBe(0);
+  });
+});
+
+describe("multiplicative functions", () => {
+  test("multiplies numbers correctly", () => {
+    expect(multiply(10, 10)).toBe(100);
+  });
+
+  test("divides numbers correctly", () => {
+    expect(divide(10, 20)).toBe(0.5);
+  });
 });
