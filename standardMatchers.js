bf2adf81f6f6fc5333fc4fa4e650ed2bf13a9c24
Selchuk Karakus <selchuk.karakus@gmail.com>
2022-11-27 21:38:45 +0000

    chore: Write a welcomeTo function and objects to pass the tests

--- standardMatchers.js
+++ standardMatchers.js
@@ -1,3 +1,31 @@
 export const compares = (firstNum, secondNum) => {
   return firstNum === secondNum ? firstNum : 0;
 };
+
+export const country1 = {
+  name: "Turkiye",
+  population: 90000000
+}
+
+export const country2 = {
+  name: "Turkiye",
+  population: 90000000,
+};
+
+export const welcomeTo = (country) => {
+  return `Welcome to ${country}`
+}
+
+export const carOnSale = {
+  make: "Mazda",
+  model: "CX30",
+  fuelType: "hybrid",
+  colour: "red"
+}
+
+export const carDesired = {
+  make: "Mazda",
+  model: "CX30"
+}
+
+
