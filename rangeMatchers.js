2a6e8aa38cf5349b918da43c420944b979f667cd
Selchuk Karakus <selchuk.karakus@gmail.com>
2022-11-28 22:03:56 +0000

    chore: Write a greaterThanOrEqual func that returns the length of a word

--- rangeMatchers.js
+++ rangeMatchers.js
@@ -1,4 +1,9 @@
 export const lessThan = (numFirst, numSecond) => {
   let isLessThan = numFirst > numSecond ? numSecond : numFirst;
   return isLessThan;
-}
+};
+
+export const greaterThanOrEqual = (word) => {
+  let wordLength = word.length;
+  return wordLength;
+};
