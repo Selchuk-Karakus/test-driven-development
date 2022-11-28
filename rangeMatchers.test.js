067b9f0767877f4ba4da668c322f4624774398d1
Selchuk Karakus <selchuk.karakus@gmail.com>
2022-11-28 21:59:24 +0000

    test: Add test to check numbers equal to or greater a range

--- rangeMatchers.test.js
+++ rangeMatchers.test.js
@@ -1,8 +1,11 @@
-import {lessThan} from "./rangeMatchers"
+import { lessThan, greaterThanOrEqual } from "./rangeMatchers";
 
 describe("matchers for range", () => {
   test("toBeLessThan() test for < ", () => {
-    expect(lessThan(5, 1)).toBeLessThan(5)
-  })
+    expect(lessThan(5, 1)).toBeLessThan(5);
+  });
 
-})
+  test("toBeGreaterThanOrEqual() tests for >=", () => {
+    expect(greaterThanOrEqual("selchuk")).toBeGreaterThanOrEqual(7);
+  });
+});
