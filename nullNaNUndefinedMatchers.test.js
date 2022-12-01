80e5a78044a735007f9d6f36bf8a910726910650
Selchuk Karakus <selchuk.karakus@gmail.com>
2022-12-01 19:46:38 +0000

    test: Add test to see if a property is defined

--- nullNaNUndefinedMatchers.test.js
+++ nullNaNUndefinedMatchers.test.js
@@ -1,4 +1,4 @@
-import { getVowels } from "./nullNaNUndefinedMatchers";
+import { getVowels, isItDefined } from "./nullNaNUndefinedMatchers";
 
 describe("matchers for null, undefined, or NaN", () => {
   test("toBeNull() tests for null", () => {
@@ -7,4 +7,7 @@ describe("matchers for null, undefined, or NaN", () => {
   test("toBeNull() tests for null", () => {
     expect(getVowels("karakus")).not.toBeNull();
   });
+  test("toBeDefined() tests if a property is defined", () => {
+    expect(isItDefined({name: "Selchuk", age: 18})).toBeDefined()
+  })
 });
