2d014c1cc2848b78000b4507d952ffc386b1255e
Selchuk Karakus <selchuk.karakus@gmail.com>
2022-12-02 20:31:28 +0000

    test: Add a test to check if an arguments squareroot is a NaN or not

--- nullNaNUndefinedMatchers.test.js
+++ nullNaNUndefinedMatchers.test.js
@@ -1,4 +1,4 @@
-import { getVowels, isItDefined } from "./nullNaNUndefinedMatchers";
+import { getVowels, isItDefined, isANumber } from "./nullNaNUndefinedMatchers";
 
 describe("matchers for null, undefined, or NaN", () => {
   test("toBeNull() tests for null", () => {
@@ -9,5 +9,9 @@ describe("matchers for null, undefined, or NaN", () => {
   });
   test("toBeDefined() tests if a property is defined", () => {
     expect(isItDefined({name: "Selchuk", age: 18})).toBeDefined()
+  })
+  test("toBeNaN() test for NaN", () => {
+    expect(isANumber(-25)).toBeNaN();
+    expect(isANumber(25)).not.toBeNaN()
   })
 });
